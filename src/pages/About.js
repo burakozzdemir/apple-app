27edb8f29daab93236baa7ea44c00d1f17f8eead
burakozzdemir <burakozdemir791@gmail.com>
2022-08-24 17:44:16 +0300

    Update about page and ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,40 +1,47 @@
 import * as React from "react";
+import appleImageSeven from "../assets/images/apple-image-seven.png";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import appleImageSeven from "../assets/images/apple-image-seven.png";
-import AppleIcon from "@mui/icons-material/Apple";
+//  import AppleIcon from "@mui/icons-material/Apple";
+
 
 const About = () => {
   return (
-    <div className="about">
-      <Card sx={{ maxWidth: 700 }}>
-        <CardActionArea>
-          <CardMedia component="img" height="250" image={appleImageSeven} />
-          <CardContent sx={{ background: "whitesmoke" }}>
-            <Typography
-              gutterBottom
-              variant="h4"
-            >
-              <AppleIcon sx={{ fontSize: "3.5rem" }} />
-            </Typography>
-            <Typography variant="body3" fontSize={17} color="text.secondary">
-              Apple Inc. is an American multinational technology company that
-              specializes in consumer electronics, software and online services
-              headquartered in Cupertino, California, United States. Apple is
-              the largest technology company by revenue billion in 2 and as of
-              May 2022, it is the world's second biggest company by market
-              capitalization, the fourth-largest personal computer vendor by
-              unit sales and second-largest mobile phone manufacturer. It is one
-              of the Big Five American information technology companies,
-              alongside Alphabet, Amazon, Meta, Microsoft.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+    <>
+      <div className="col-12 text-center">
+        <h2 className="my-3">About</h2>
+        <hr />
+      </div>
+      <div className="about_card">
+        <Card sx={{ maxWidth: 700 }}>
+          <CardActionArea>
+            <CardMedia component="img" height="380" image={appleImageSeven} />
+            <CardContent sx={{ background: "whitesmoke" }}>
+              <Typography
+                gutterBottom
+                variant="h4"
+              >
+                {/* <AppleIcon sx={{ fontSize: "3.5rem" }} /> */}
+              </Typography>
+              <Typography variant="body3" fontSize={16} color="text.secondary">
+                Apple Inc. is an American multinational technology company that
+                specializes in consumer electronics, software and online services
+                headquartered in Cupertino, California, United States. Apple is
+                the largest technology company by revenue billion in 2 and as of
+                May 2022, it is the world's second biggest company by market
+                capitalization, the fourth-largest personal computer vendor by
+                unit sales and second-largest mobile phone manufacturer. It is one
+                of the Big Five American information technology companies,
+                alongside Alphabet, Amazon, Meta, Microsoft. <br /> Apple became the first publicly traded U.S. company to be valued at over $1 trillion in August 2018, and most recently $3 trillion in January 2022.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    </>
   );
 };
 
